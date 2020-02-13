44e45095894305eb463c7737c7249077584e063a
bobriwe <kirill.madze@gmail.com>
2020-02-13 12:42:39 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const Subscript = require('@bobrovice/subscript');
+const EditorJS = require('@editorjs/editorjs');
+
+const editor = new EditorJS({
+    autofocus: true,
+    tools: {
+      subscript: {
+          class: Subscript
+      }
+    }
+  });
+
+  document.getElementById('save-button').onclick = () => {
+      editor.save().then((outputData) => {
+      console.log('Article data: ', outputData)
+      }).catch((error) => {
+      console.log('Saving failed: ', error)
+      });
+  }
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+      rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [
+              {
+                loader: 'babel-loader',
+                query: {
+                  presets: [ '@babel/preset-env' ],
+                },
+              },
+            ]
+        },
+        {
+            test: /\.css$/,
+            use: [
+              'style-loader',
+              'css-loader'
+            ]
+          },
+      ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+      template: './index.html'
+  })]
+};
